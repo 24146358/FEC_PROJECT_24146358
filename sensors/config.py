ee326b41b5b11fde0ce8d93dc068315855f94763
24146358 <x24146358@student.ncirl.ie>
2026-07-22 15:29:12 +0100

    Elastic IP attached

--- sensors/config.py
+++ sensors/config.py
@@ -1,5 +1,5 @@
 # Central config — change FOG_URL to your EC2 public IP once deployed
-FOG_URL = "http://54.198.36.155:5000/ingest"  # change to EC2 IP later
+FOG_URL = "http://54.156.77.3:5000/ingest"  # change to EC2 IP later
 DATASET_PATH = "../data/iot_telemetry_data.csv"
 DEFAULT_INTERVAL = 2  # seconds between readings — configurable at runtime
 BATCH_DEVICE_ID = "b8:27:eb:bf:9d:51"  # use one device from dataset
