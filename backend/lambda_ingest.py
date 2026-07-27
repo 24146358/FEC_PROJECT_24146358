2797048bd279db10e8192d18b020398987cefed0
24146358 <x24146358@student.ncirl.ie>
2026-07-27 14:41:48 +0100

    lambda function name

--- backend/lambda_ingest.py
+++ backend/lambda_ingest.py
@@ -5,7 +5,7 @@ import time
 dynamodb = boto3.resource("dynamodb", region_name="eu-west-1")
 table = dynamodb.Table("FEC_SensorReadings")
 
-# IoT ingest Lambda it receives fog node batches and writes to Dynamodb table
+# IoT ingest Lambda it receives fog node batches and writes to Dynamodb tables
 def lambda_handler(event, context):
     try:
         body = json.loads(event.get("body", "{}"))
